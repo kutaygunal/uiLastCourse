58b223894367aecc70132df3d77f868f6f37ecde
Kutay Gunal <kutaygunal@outlook.com>
2020-12-23 12:46:02 -0600

    First Commit

---
+++ UI_Class/Assets/Scripts/Coin.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour 
+{
+	
+	void Awake()
+	{
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "Player")
+			print ("You picked up a coin!");
+		
+		//Set the coin to inactive
+		gameObject.SetActive(false);
+	}
+
+}
---
+++ UI_Class/Assets/Scripts/GameState.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameState : MonoBehaviour 
+{
+	private GameObject[] coins;
+	public int totalCoins;
+
+
+	public bool gameRunning = false;
+
+	// Use this for initialization
+	void Awake () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	public void StartGame()
+	{
+		gameRunning = true;
+	}
+
+	public void GameOver()
+	{
+		gameRunning = false;
+		print ("Game Over!");
+	}
+}
