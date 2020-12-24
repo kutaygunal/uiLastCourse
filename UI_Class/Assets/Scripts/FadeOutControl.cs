b9153e05c419ff8cc0d836f8ce87f034557bde17
Kutay Gunal <kutaygunal@outlook.com>
2020-12-23 23:23:48 -0600

    Page 65

---
+++ UI_Class/Assets/Scripts/FadeOutControl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+public class FadeOutControl : MonoBehaviour
+{
+    private Animator menuAnim;
+    void Awake()
+    {
+        menuAnim = GetComponent<Animator>();
+    }
+    public void MenuFade()
+    {
+        menuAnim.SetTrigger("FadeOut");
+    }
+}
