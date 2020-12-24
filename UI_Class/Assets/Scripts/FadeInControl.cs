20877e9ea48c7c19fee3b9bab1a7709ef00cf03d
Kutay Gunal <kutaygunal@outlook.com>
2020-12-23 23:39:08 -0600

    Page 75

---
+++ UI_Class/Assets/Scripts/FadeInControl.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+public class FadeInControl : MonoBehaviour
+{
+    private Animator hudAnim;
+    void Awake()
+    {
+        hudAnim = GetComponent<Animator>();
+    }
+    public void HUDFade()
+    {
+        hudAnim.SetTrigger("FadeIn");
+    }
+}
